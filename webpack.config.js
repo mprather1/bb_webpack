8374622cf217b7651964cca2f549d8c4f9fd8d3e
Michael Prather <mprather1@gmail.com>
2016-10-28 12:01:37 -0500

    plugins in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,18 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: 'jquery',
       _: 'underscore'
-    })  
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      mangle: true,
+      sourcemap: false,
+      beautify: false,
+      dead_code: true
+    })
   ]
 };
