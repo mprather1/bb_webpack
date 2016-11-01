81966d60929e23fac493ff9b2ef62a5bf2d6000c
Michael Prather <mprather1@gmail.com>
2016-10-31 19:27:40 -0500

    validations work

--- app/models/User.js
+++ app/models/User.js
@@ -1,5 +1,39 @@
+var validation = require("backbone.validation");
+_.extend(Backbone.Validation.callbacks, {
+  valid: function (view, attr, selector) {
+    var $el = view.$('[name=' + attr + ']'), 
+        $group = $el.closest('.form-group');
+    $group.removeClass('has-error');
+    $group.find('.help-block').html('').addClass('hidden');
+  },
+  invalid: function (view, attr, error, selector) {
+    console.log('invalid')
+    var $el = view.$('[name=' + attr + ']'), 
+      $group = $el.closest('.form-group');
+    $group.addClass('has-error');
+    $group.find('.help-block').html(error).removeClass('hidden');
+  }
+});
 var User = Backbone.Model.extend({
-  urlRoot: 'http://localhost:8000/api/users'
+  urlRoot: 'http://localhost:8000/api/users',
+  validation: {
+    firstName: {
+      required: true
+    },
+    lastName: {
+      required: true
+    },
+    email: {
+      required: true,
+      pattern: 'email'
+    },
+    phone: {
+      required: true,
+      pattern: 'number',
+      minLength: 10,
+      maxLength: 10
+    }
+  },
 });
 
 module.exports = User;
--- app/views/UsersForm.js
+++ app/views/UsersForm.js
@@ -1,24 +1,45 @@
+String.prototype.capitalizedFirstLetter = function(){
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+_.extend(Backbone.Validation.callbacks, {
+  valid: function (view, attr, selector) {
+    var $el = view.$('[name=' + attr + ']'), 
+        $group = $el.closest('.form-group');
+    $group.removeClass('has-error');
+    $group.find('.help-block').html('').addClass('hidden');
+  },
+  invalid: function (view, attr, error, selector) {
+    var $el = view.$('[name=' + attr + ']'), 
+      $group = $el.closest('.form-group');
+    $group.addClass('has-error');
+    $group.find('.help-block').html(error).removeClass('hidden');
+  }
+});
+
 var UsersFormView = Backbone.Marionette.View.extend({
   template: require("../templates/users-form-template.html"),
   initialize: function(){
+    Backbone.Validation.bind(this, {
+      model: this.model
+    });
     this.listenTo(Backbone, 'form:submit', this.submitUsersForm)
   },
   submitUsersForm: function(){
     var userAttrs = {
-      firstName: $('#firstName_input').val(),
-      lastName: $('#lastName_input').val(),
-      email: $('#email_input').val(),
+      firstName: $('#firstName_input').val().capitalizedFirstLetter(),
+      lastName: $('#lastName_input').val().capitalizedFirstLetter(),
+      email: $('#email_input').val().toLowerCase(),
       phone: $('#phone_input').val()
     };
     this.model.set(userAttrs);
-    // if(this.model.isValid(true)){
+    if(this.model.isValid(true)){
       this.model.save();
       this.collection.add(this.model);
-      // Backbone.Validation.unbind(this);
+      Backbone.Validation.unbind(this);
       Backbone.trigger('form:cancel')
-    // }
+    }
   }
-  
 });
 
 module.exports = UsersFormView;
